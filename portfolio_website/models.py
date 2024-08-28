fe68f8beb5bf93d60053214a77b9c75a3b1bdde9
Mahdi <mahdimeyghani02@gmail.com>
2024-08-28 09:55:54 +0330

    Create a model for projects

--- portfolio_website/models.py
+++ portfolio_website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField()
+    image = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
