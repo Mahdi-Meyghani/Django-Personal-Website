f15faa958678fbbbe81cf044b3539bdf97f2ae03
Mahdi <mahdimeyghani02@gmail.com>
2024-08-28 17:26:50 +0330

    Add projects to home page but with some bugs

--- portfolio_website/views.py
+++ portfolio_website/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Project
 
 
 def home(request):
-    return render(request, 'portfolio_website/home.html')
+    projects_left = Project.objects.all()[:10]  # Get projects 1 to 10
+    projects_right = Project.objects.all()[10:20]  # Get projects 11 to 20
+    return render(request, 'portfolio_website/home.html', {
+        'projects_left': projects_left,
+        'projects_right': projects_right
+    })
