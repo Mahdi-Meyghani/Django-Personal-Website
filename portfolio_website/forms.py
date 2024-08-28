c549d73417b520b2da1b725ce5f8916e21fb3bed
Mahdi <mahdimeyghani02@gmail.com>
2024-08-29 00:55:13 +0330

    Send email in contact page

--- portfolio_website/forms.py
+++ portfolio_website/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=500)
     email = forms.EmailField()
-    message = forms.CharField(max_length=5000)
+    message = forms.CharField(max_length=5000, widget=forms.Textarea)
--- portfolio_website/views.py
+++ portfolio_website/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
 from .models import Project
+from . import forms
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 
 def home(request):
@@ -12,4 +17,25 @@ def home(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        try:
+            validate_email(email)
+            body = f"From: {email}\n\n{name.title()}\n{message}"
+            email_message = EmailMessage(
+                subject='Portfolio Django Website Email',
+                body=body,
+                to=['portfolio.websate@gmail.com'],
+            )
+            email_message.send()
+            messages.success(request, 'Your email has been sent successfully. Thanks 😄')
+            return redirect('contact')
+        except ValidationError:
+            messages.error(request, 'Invalid email address. 😣')
+        except Exception:
+            messages.error(request, 'Failed to send email. Try again. 😢')
+
     return render(request, 'portfolio_website/contact.html')
